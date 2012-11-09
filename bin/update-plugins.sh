94304594264dff7a17f4d33f1a71da0b1f773828
moljac024 <matic.bojan@gmail.com>
2012-11-09 15:28:50 +0100

    Updated submodules and removed command-t

--- bin/update-plugins.sh
+++ bin/update-plugins.sh
@@ -5,6 +5,6 @@ cd ~/.vim
 git submodule foreach git pull origin master
 
 # Compile command-t extension
-cd .vim/bundle/command-t/ruby/command-t
-ruby extconf.rb && make && make clean && make distclean &&\
-echo "Compiled command-t c extension succesfully!"
+#cd .vim/bundle/command-t/ruby/command-t
+#ruby extconf.rb && make && make clean && make distclean &&\
+#echo "Compiled command-t c extension succesfully!"
